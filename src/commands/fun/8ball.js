6913f2b1897a5d7375fa93928ea3034d5e820da3
zema-tech <alessio.zemignani@gmail.com>
2026-09-16 20:54:30 +0200

    Add 8ball command

---
+++ src/commands/fun/8ball.js
@@ -0,0 +1,51 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+
+const risposte = [
+  'Sì, assolutamente.',
+  'È deciso così.',
+  'Senza dubbio.',
+  'Sì, decisamente.',
+  'Puoi contarci.',
+  'Come la vedo io, sì.',
+  'Molto probabilmente.',
+  'Le prospettive sono buone.',
+  'Sì.',
+  'I segnali puntano al sì.',
+  'Risposta confusa, riprova.',
+  'Chiedi più tardi.',
+  'Meglio non dirtelo ora.',
+  'Non posso prevederlo ora.',
+  'Concentrati e riprova.',
+  'Non contarci.',
+  'La mia risposta è no.',
+  'Le mie fonti dicono no.',
+  'Le prospettive non sono così buone.',
+  'Molto dubbioso.',
+];
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('8ball')
+    .setDescription('Chiedi qualcosa alla palla magica 8')
+    .addStringOption(option =>
+      option.setName('domanda')
+        .setDescription('La tua domanda')
+        .setRequired(true)
+    ),
+  cooldown: 3,
+  async execute(interaction) {
+    const domanda = interaction.options.getString('domanda');
+    const risposta = risposte[Math.floor(Math.random() * risposte.length)];
+
+    const embed = new EmbedBuilder()
+      .setColor(0x000000)
+      .setTitle('🎱 Palla Magica 8')
+      .addFields(
+        { name: 'Domanda', value: domanda },
+        { name: 'Risposta', value: risposta }
+      )
+      .setFooter({ text: `Richiesto da ${interaction.user.username}` });
+
+    await interaction.reply({ embeds: [embed] });
+  },
+};
